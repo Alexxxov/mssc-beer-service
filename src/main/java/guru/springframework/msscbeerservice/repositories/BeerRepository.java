774f7a73a36679ec56a9e80a7b41e7ef0c9d0f50
alexxxov1 <73472823+alexxxov1@users.noreply.github.com>
2021-05-15 21:22:57 +0300

    added get beer by upc

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -16,5 +16,7 @@ public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
 
+    Beer findByUpc(String upc);
+
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
 }
